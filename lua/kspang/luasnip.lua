81ae1b14c403e28ec867d1e8fcce0d0592b5f21e
kspang-ss <kspang@spangsoftware.com>
2026-06-09 06:42:51 -0400

    add dynamic function snippet

--- lua/kspang/luasnip.lua
+++ lua/kspang/luasnip.lua
@@ -11,8 +11,60 @@ local f = ls.function_node
 local d = ls.dynamic_node
 local sn = ls.snippet_node
 
-local css_snippets = {
+-- Helper to generate comment lines dynamically based on function arguments
+local function generate_comment_nodes(args)
+    local func_args = args[1][1] or ""
+    local return_type = args[2][1] or ""
+    local nodes = {}
 
+    -- Split args
+    local args_table = {}
+    for arg in string.gmatch(func_args, "([^,]+)") do
+        table.insert(args_table, arg)
+    end
+
+    -- Format comment
+    table.insert(nodes, t({ "/**", " * " }))
+    table.insert(nodes, i(1, "Description"))
+    table.insert(nodes, t({ "", " *" }))
+
+    -- Add @param for args
+    local last_index = 0
+    for index, arg_name in ipairs(args_table) do
+        table.insert(nodes, t({"", " * @param " .. arg_name .. " - " }))
+        table.insert(nodes, i(index + 1))
+        last_index = index + 1
+    end
+
+    -- Only add @returns if not void
+    if return_type ~= "void" then
+        table.insert(nodes, t({"", " * @returns " .. return_type .. " - "}))
+        table.insert(nodes, i(last_index + 1))
+    end
+
+    table.insert(nodes, t({"", " */" }))
+    return ls.snippet_node(nil, nodes)
+end
+
+ls.add_snippets("all", {
+    s("fn", {
+        -- update comments :)
+        d(4, generate_comment_nodes, { 3, 1 }),
+        t({ "", "" }),
+
+        i(1, "type"),
+        t(" function "),
+        i(2, "functionName"),
+        t("("),
+        i(3, "args"),
+        t(") {"),
+        t({ "", "    " }),
+        i(0),
+        t({ "", "}" }),
+    }),
+})
+
+local css_snippets = {
     s("cl", fmt([[
     .<> {
         <>
@@ -37,31 +89,31 @@ local css_snippets = {
 
     s("ts", {
         t('transition: var(--trans);'),
-    })
+    }),
 }
 local html_snippets = {
 
     s("if", fmt([[
     <?php if ({}): ?>
-        {}
+    {}
     <?php endif; ?>
     ]], { i(1), i(2) })),
 
     s("foreach", fmt([[
     <?php foreach ({} as $key => $value): ?>
-        {}
+    {}
     <?php endforeach; ?>
     ]], { i(1), i(2) })),
 
     s("section", fmt([[
     <section class="{}">
-        {}
+    {}
     </section>
     ]], { i(1), i(2) })),
 
     s("div", fmt([[
     <div{}>
-        {}
+    {}
     </div>
     ]], { i(1), i(2) })),
 
